c487d10488aca24a990db7ca283991d99e8002e5
Don Widman <don.widman@gmail.com>
2013-03-17 13:06:56 -0700

    Revision 4
    bugs

--- minecraft_tempfile.py
+++ minecraft_tempfile.py
@@ -1,6 +1,6 @@
 # Manages the temp file for locations
 def read_file(file):
-    temp_temp = open(temp_file).readlines()
+    temp_temp = open(file).readlines()
     temp_list = [i.strip() for i in temp_temp]
     print temp_list
     return temp_list
